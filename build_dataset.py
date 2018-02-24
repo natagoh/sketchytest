39eda94538d466b9073fade7639da98b5d98d3a5
quietpirate <mlu355@gmail.com>
2018-02-23 23:41:54 -0800

    Split data into 80/10/10

--- build_dataset.py
+++ build_dataset.py
@@ -21,6 +21,7 @@ import argparse
 import random
 import os
 import platform
+import shutil
 
 from PIL import Image
 from tqdm import tqdm
@@ -66,6 +67,59 @@ def compile_images(data_dir, classes, dst_dir):
                 resize_and_save(src, dst_dir, new_name)
         print('done renaming imgs in class: ', clas)
 
+# Splits data into train, dev, and test set with an 80/10/10 split. This is because we have 20,000 images so 
+# this is a reasonable datasplitting scheme. 
+def split_data(dst_data_dir):
+    # Get train, test, and val directory names. 
+    train_data_dir = os.path.join(dst_data_dir, 'train_sketches')
+    test_data_dir = os.path.join(dst_data_dir, 'test_sketches')
+    val_data_dir = os.path.join(dst_data_dir, 'val_sketches')
+
+    # Get the filenames in each directory (train and test)
+    filenames = os.listdir(train_data_dir)
+    #filenames = [os.path.join(train_data_dir, f) for f in filenames if f.endswith('.png')]
+    filenames = [f for f in filenames if f.endswith('.png')]
+
+    # Split the images in 'train_signs' into 80% train, 10% val and 10% test
+    # Make sure to always shuffle with a fixed seed so that the split is reproducible
+    random.seed(230)
+    filenames.sort()
+    random.shuffle(filenames)
+
+    split1 = int(0.8 * len(filenames))
+    split2 = int(0.9 * len(filenames))
+
+    train_filenames = filenames[:split1]
+    val_filenames = filenames[split1:split2]
+    test_filenames = filenames[split2:]
+
+    filenames = {'train': train_filenames,
+                 'val': val_filenames,
+                 'test': test_filenames}
+
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+    else:
+        print("Warning: output dir {} already exists".format(args.output_dir))
+
+    # Preprocess train, val and test
+    for split in ['train', 'val', 'test']:
+        output_dir_split = os.path.join(args.output_dir, '{}_sketches'.format(split))
+        if not os.path.exists(output_dir_split):
+            os.mkdir(output_dir_split)
+        else:
+            print("Warning: dir {} already exists".format(output_dir_split))
+
+        print("Processing {} data, saving preprocessed data to {}".format(split, output_dir_split))
+        for filename in tqdm(filenames[split]):
+            save_path = os.path.join(output_dir_split, filename)
+            #print(save_path)
+            shutil.move(os.path.join(train_data_dir, filename), save_path)
+            #print(split, filename, output_dir_split)
+            #image = Image.open(os.path.join(train_data_dir, filename))
+            #save_path = os.path.join(output_dir_split, filename)
+            #print(save_path)
+            #image.save(save_path)
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -84,47 +138,10 @@ if __name__ == '__main__':
     dst_data_dir = os.path.join(par_dir, '64x64_SKETCHES')
     if not os.path.exists(dst_data_dir):
         os.makedirs(dst_data_dir)
-    compile_images(args.data_dir, classes, os.path.abspath(dst_data_dir))
+    train_data_dir = os.path.join(dst_data_dir, 'train_sketches')
+    os.mkdir(train_data_dir)
+    compile_images(args.data_dir, classes, os.path.abspath(train_data_dir))
 
-    '''
-
-    # Get the filenames in each directory (train and test)
-    filenames = os.listdir(train_data_dir)
-    filenames = [os.path.join(train_data_dir, f) for f in filenames if f.endswith('.jpg')]
-
-    test_filenames = os.listdir(test_data_dir)
-    test_filenames = [os.path.join(test_data_dir, f) for f in test_filenames if f.endswith('.jpg')]
-
-    # Split the images in 'train_signs' into 80% train and 20% val
-    # Make sure to always shuffle with a fixed seed so that the split is reproducible
-    random.seed(230)
-    filenames.sort()
-    random.shuffle(filenames)
-
-    split = int(0.8 * len(filenames))
-    train_filenames = filenames[:split]
-    val_filenames = filenames[split:]
-
-    filenames = {'train': train_filenames,
-                 'val': val_filenames,
-                 'test': test_filenames}
-
-    if not os.path.exists(args.output_dir):
-        os.mkdir(args.output_dir)
-    else:
-        print("Warning: output dir {} already exists".format(args.output_dir))
-
-    # Preprocess train, val and test
-    for split in ['train', 'val', 'test']:
-        output_dir_split = os.path.join(args.output_dir, '{}_signs'.format(split))
-        if not os.path.exists(output_dir_split):
-            os.mkdir(output_dir_split)
-        else:
-            print("Warning: dir {} already exists".format(output_dir_split))
-
-        print("Processing {} data, saving preprocessed data to {}".format(split, output_dir_split))
-        for filename in tqdm(filenames[split]):
-            resize_and_save(filename, output_dir_split, size=SIZE)
+    split_data(dst_data_dir)
 
     print("Done building dataset")
-    '''
